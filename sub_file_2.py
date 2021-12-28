3b49d0140f6e8185d8c8770b09f4540233162c93
Shuka <89642475+Shchukaa@users.noreply.github.com>
2021-12-28 15:56:35 +0300

    ver_smth
    Добавлена моделька огненного врага и его передвижение
    -Разобратсья, как добавлять сразу несколько врагов(чтобы они были не в одном пикселе, а с некоторым расстоянием друг от друга)
    -Добавить полоску хп
    -Добавить меню с выбором нужной башни
    -Когда выбираешь башню - появляется сетка поля

--- sub_file_2.py
+++ sub_file_2.py
@@ -16,6 +16,18 @@ clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 towers_group = pygame.sprite.Group()
+enemies_group = pygame.sprite.Group()
+
+
+def load_tiles(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    image = image.convert_alpha()
+    return image
 
 
 def load_image(name, color_key=None):
@@ -25,12 +37,8 @@ def load_image(name, color_key=None):
     except pygame.error as message:
         print('Cannot load image:', name)
         raise SystemExit(message)
-
-    if color_key == -1 or name == 'fire_tower.png':
-        color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    else:
-        image = image.convert_alpha()
+    color_key = image.get_at((0, 0))
+    image.set_colorkey(color_key)
     return image
 
 
@@ -81,28 +89,6 @@ class Tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
-class Tower(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y):
-        super().__init__(all_sprites)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.width * i, self.rect.height * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
-
-
 class Board:
     def __init__(self, width, height):
         self.width = width
@@ -126,13 +112,84 @@ class Board:
         return self.on_click(cell)
 
 
-tile_images = {'grass': load_image('grass.png'), 'road_g': load_image('road_g.png'), 'road_v': load_image('road_v.png'),
-               'road_1': load_image('road_1.png'), 'road_2': load_image('road_2.png'), 'road_3':
-                   load_image('road_3.png'), 'road_4': load_image('road_4.png')}
+class Tower(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.width * i, self.rect.height * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(enemies_group, all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.turn = 1
+        self.win = False
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.width * i, self.rect.height * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]
+        if (self.rect.x, self.rect.y) == level_1[5] and not self.win:
+            enemies_group.remove(fire_enemy)
+            print('вы проиграли')
+            self.win = True
+        elif not self.win:
+            turn = level_1[self.turn - 1]
+            next_turn = level_1[self.turn]
+            if turn[0] < next_turn[0]:
+                self.rect = self.rect.move(5, 0)
+            elif turn[0] > next_turn[0]:
+                self.rect = self.rect.move(-5, 0)
+            elif turn[1] < next_turn[1]:
+                self.rect = self.rect.move(0, 5)
+            elif turn[1] > next_turn[1]:
+                self.rect = self.rect.move(0, -5)
+            if (self.rect.x, self.rect.y) == level_1[self.turn]:
+                self.turn += 1
+        else:
+            pass
+
+
+tile_images = {'grass': load_tiles('grass.png'), 'road_g': load_tiles('road_g.png'), 'road_v': load_tiles('road_v.png'),
+               'road_1': load_tiles('road_1.png'), 'road_2': load_tiles('road_2.png'), 'road_3':
+                   load_tiles('road_3.png'), 'road_4': load_tiles('road_4.png')}
 tile_width = tile_height = 48
 
 board = Board(10, 10)
 generate_level(load_level('level_1.txt'))
+# Spawn Enemies
+# Координаты поворотов должны быть кратны скорости передвижения(5)
+level_1 = [(0, 85), (335, 85), (335, 320), (235, 320), (235, 230), (0, 230)]
+for i in range(1):
+    fire_enemy = Enemy(load_image('fire_enemy.png'), 4, 1, level_1[0][0], level_1[0][1])
+    enemies_group.add(fire_enemy)
 running = True
 while running:
     for event in pygame.event.get():
@@ -140,13 +197,15 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = board.get_click(event.pos)
-            fire_tower = Tower(load_image('fire_tower.png'), 6, 1, (x - 1) * 48, (y - 1) * 48)
-            all_sprites.add(fire_tower)
+            if event.button == 1:
+                fire_tower = Tower(load_image('fire_enemy.png'), 4, 1, x * 48, y * 48)
+                towers_group.add(fire_tower)
     screen.fill(pygame.Color(0, 0, 0))
     tiles_group.draw(screen)
-    all_sprites.draw(screen)
+    towers_group.draw(screen)
+    enemies_group.draw(screen)
     pygame.display.flip()
-    clock.tick(10)
+    clock.tick(7)
     all_sprites.update()
 
 terminate()
